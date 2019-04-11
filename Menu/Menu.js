4766189aed1ff0df509e194f2386b0a4798b2a49
Daisy Mesa <Daisy@DMM.fios-router.home>
2019-04-11 08:52:50 -0400

    Made updates to Article and Menu files

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,11 +4,18 @@ const toggleMenu = () => {
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu = document.querySelectorAll(".menu");
+const menu = document.querySelector('.menu');
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 
  // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', this.toggle);
+menuButton.addEventListener('click', toggleMenu);
 
+// const toggleMenu = () => {
+//   menu.classList.toggle('menu--open')
+// }
+
+// const menu = document.querySelector('.menu');
+// const menuButton = document.querySelector('.menu-button');
+// menuButton.addEventListener('click', toggleMenu);
